54c0ba8921463735fbec91f5ef4a1a6a8a8076d4
Vikas Baswapuram <S538336@nwmissouri.edu>
2020-10-23 11:31:17 -0500

    Project initialized

---
+++ ArithmeticsCalculator/ArithmeticsCalculator/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ArithmeticsCalculator
+//
+//  Created by student on 10/23/20.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
