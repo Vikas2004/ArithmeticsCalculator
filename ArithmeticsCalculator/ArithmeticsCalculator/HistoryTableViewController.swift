3b23715b6af661a6a259d92c9ff9bb40b4acdccc
Harish Thadka <S538362@nwmissouri.edu>
2020-11-25 18:14:07 -0600

    adding comments

--- ArithmeticsCalculator/ArithmeticsCalculator/HistoryTableViewController.swift
+++ ArithmeticsCalculator/ArithmeticsCalculator/HistoryTableViewController.swift
@@ -10,27 +10,27 @@ import UIKit
 
 class HistoryTableViewController: UITableViewController {
 
-    // Creating local guess array to store guesses array
+    // Creating local history array to store history array
     private var historyResults:[ArithCalculator.Result] = []
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        // Making empty array to add new elements of guesses
+        // Making empty array to add new elements of history
         historyResults = []
-        // addding elements into historyGuesses array by calling history function
+        // addding elements into historyResults array by calling history function
         history()
         // Reloading data after retrieve data
         self.tableView.reloadData()
 
     }
 
-    // This function is used to retrieve data from guesser and store in local variable
+    // This function is used to retrieve data from results and store in local variable
     func history() {
         
         
         let num:Int = ArithCalculator.shared.resultsCount();
         print("count \(num)");
-        // iterate through guesses array and appendin elements to local array
+        // iterate through historyResults array and adding elements to local array
         for i in stride(from: 0, to: num, by: 1){
             historyResults.append(ArithCalculator.shared.results(index: i))
         }
@@ -43,14 +43,14 @@ class HistoryTableViewController: UITableViewController {
         return 1
     }
 
-    // Returns count of historyGuesses array i.e; number of cells
+    // Returns count of historyResults array i.e; number of cells
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return historyResults.count
     }
     
     
-    // This function display required rows of historyGuesses
+    // This function display required rows of historyResults
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cellIdentifier = "cellIdentifier"
